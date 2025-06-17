f1b9fb91b9e57d14e27a1ad6fd80613f160a1361
gkdenizylmaz <gkdeniz6106@gmail.com>
2025-06-17 09:42:15 +0300

    Remove weatherForecast Ex

--- obj/Debug/net9.0/TRT-backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/TRT-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TRT-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bd76215ac4d38115347dcf085a6e47d3ae33a1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97208893f63921b6d9d7758fee272d9ac2452fad")]
 [assembly: System.Reflection.AssemblyProductAttribute("TRT-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TRT-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
